7effdefdf2291e39418df8e2d22ae614a5cfe71f
Sam Silver <sam55silver@gmail.com>
2025-07-18 15:52:32 -0300

    clean up to just client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import React from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
+import { Toaster } from "./components/ui/toaster";
+import NotFound from "./pages/not-found";
+import Home from "./pages/Home";
 
 function Router() {
   return (
     <Switch>
-      <Route path="/thalios/*" component={Home} />
+      <Route path="/*" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
